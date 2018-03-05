bd4e73c12f0e886a0f45d49fdbd860be92295866
Christof Harnischmacher <ch@cm4all.com>
2018-03-05 02:44:13 +0100

    renamed FPGA-... to Core, removed old cores

--- FPGA/counter.v
+++
@@ -1,8 +0,0 @@
-module counter (input clock, output reg [31:0] counter);
-
-	always @ (posedge clock)// on positive clock edge
-	begin
-		counter <= #1 counter + 1;// increment counter
-	end
-endmodule// end of module counter
-	
--- FPGA/errcounter.v
+++
@@ -1,8 +0,0 @@
-module errcounter (input locked, output reg [2:0] counter);
-
-	always @ (negedge locked)// on positive clock edge
-	begin
-		counter <= #1 counter + 1'b1;// increment counter
-	end
-endmodule// end of module counter
-	
