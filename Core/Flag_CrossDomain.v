90b7d76b3f59df8f53a96c4ed6df455959e9ee6e
Christof Harnischmacher <ch@cm4all.com>
2018-09-08 13:02:07 +0200

    reset working

--- Core/Flag_CrossDomain.v
+++ Core/Flag_CrossDomain.v
@@ -7,38 +7,37 @@ module Flag_CrossDomain(
     input clkB,
     output FlagOut_clkB
 );
-    reg FlagToggle_clkA;
-    reg [2:0] SyncA_clkB;
+    // reg FlagToggle_clkA;
+    // reg [2:0] SyncA_clkB;
+
+    // always @(posedge clkA) begin
+    //     FlagToggle_clkA <= FlagToggle_clkA ^ FlagIn_clkA;  // when flag is asserted, this signal toggles (clkA domain)
+    // end
+
+    // always @(posedge clkB) begin
+    //     SyncA_clkB <= {SyncA_clkB[1:0], FlagToggle_clkA};  // now we cross the clock domains
+    // end
+
+    // assign FlagOut_clkB = (SyncA_clkB[2] ^ SyncA_clkB[1]);  // and create the clkB flag
+    reg data_out_meta;
+    reg[3:0] rCount;
+    reg[2:0] data_out_reg;
+
+    assign FlagOut_clkB = data_out_reg[2];
 
     always @(posedge clkA) begin
-        FlagToggle_clkA <= FlagToggle_clkA ^ FlagIn_clkA;  // when flag is asserted, this signal toggles (clkA domain)
+        if (FlagIn_clkA) begin
+            rCount <= 4'b1000;
+        end else if (rCount > 0) begin
+            rCount <= rCount - 1'b1;
+        end
+    end
+
+    always @(rCount) begin
+        data_out_meta <= (rCount > 0);
     end
 
     always @(posedge clkB) begin
-        SyncA_clkB <= {SyncA_clkB[1:0], FlagToggle_clkA};  // now we cross the clock domains
+        data_out_reg <= { data_out_reg[1], data_out_reg[0], data_out_meta };
     end
-
-    assign FlagOut_clkB = (SyncA_clkB[2] ^ SyncA_clkB[1]);  // and create the clkB flag
-
-// reg data_out_meta;
-// reg[3:0] rCount;
-// reg[1:0] data_out_reg;
-
-// assign FlagOut_clkB = data_out_reg[1];
-
-// always @(posedge clkA) begin
-//     if (FlagIn_clkA) begin
-//         rCount <= 4'b1000;
-//     end else if (rCount > 0) begin
-//         rCount <= rCount - 1'b1;
-//     end
-// end
-
-// always @(rCount) begin
-//     data_out_meta <= (rCount > 0);
-// end
-
-// always @(posedge clkB) begin
-//     data_out_reg <= { data_out_reg[0], data_out_meta };
-// end
 endmodule
