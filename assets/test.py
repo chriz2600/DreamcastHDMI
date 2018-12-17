1eed345f4494313274eb97bcc8fdc60089545f51
Christof Harnischmacher <ch@cm4all.com>
2018-12-17 09:20:47 +0100

    test script

--- assets/test.py
+++ assets/test.py
@@ -6,13 +6,23 @@
 # 1,06993006993007
 #   1,05893536121673
 
+results = {}
+
 for vert in range(1053, 1900):
     for horiz in range(859, 1400):
         horiztest = (horiz / 858)
         verttest = (vert / 1052)
         ref = horiztest * verttest * 54
-        if ref == 72.0:
-            print("%.60f %.60f %f %f %f" % (horiztest, verttest, ref, horiz, vert));
+        results[ref] = { 
+                "horiztest": horiztest,
+                "verttest": verttest,
+                "ref": ref,
+                "horiz": horiz,
+                "vert": vert
+            }
 
+for res in results:
+    data = results[res]
+    print("%g MHz: %f %f (%.60f %.60f)" % (res, data["horiz"], data["vert"], data["horiztest"], data["verttest"]))
 
-# ./assets/test.py | awk '{ print length, $0 }' | sort -n | cut -d" " -f2- | less
+# ./assets/test.py | awk '{ print length($1), $0 }' | sort -n | cut -d" " -f2- | head -10
